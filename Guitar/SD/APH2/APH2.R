cc07eabdc47e4db6d96282e12d0142f9f248eb19
strayr <43092584+strayr@users.noreply.github.com>
2021-07-03 07:10:17 +0100

    Make this work

--- Guitar/SD/APH2/APH2.R
+++ Guitar/SD/APH2/APH2.R
@@ -1,15 +1,16 @@
 ###
-# Each pickup needs one of these to define a report. This should be as short as possible and 
-# contain no logic beyond defining a piuckup and printing some data
+# Each pickup needs one of these to define a report. This should be as short as
+# possible and contain no logic beyond defining a pickup and printing some data
 #
- 
+
 
 ##
 # Import needed libraries
 
-#libfolder<-"pickupAnalyseR"
-source('sysCompPickup.R')
-source('pickupComparisonCharts.R')
+#Explicitly call our library folder
+libfolder <- "pickupAnalyseR-stable"
+source(paste(libfolder,"sysCompPickup.R", sep = "/"))
+source(paste(libfolder,"pickupComparisonCharts.R", sep = "/"))
 
 
 
@@ -19,41 +20,41 @@ source('pickupComparisonCharts.R')
 
 #I'm using my own pickup as the default for this template.
 
-chartHeading = "Seymour Duncan APH2"
+chartHeading <- "Seymour Duncan APH2"
 
 pickupList = c(
   SysCompPickup(
     name = "APH2 Neck",
     manuf = "Seymour Duncan",
     mDCR = 8.2,
-    tableBase = 'PublicData/Guitar/SD/APH2/APH2N',
-    LDOffset=0,
-    ULOffset=0
+    tableBase = "Guitar/SD/APH2/APH2N",
+    LDOffset = 0,
+    ULOffset = 0
   ),
   SysCompPickup(
     name = "APH2 Bridge",
     manuf = "Seymour Duncan",
     mDCR = 8.8,
-    tableBase = 'PublicData/Guitar/SD/APH2/APH2B',
-    LDOffset=0,
-    ULOffset=0
+    tableBase = "Guitar/SD/APH2/APH2B",
+    LDOffset = 0,
+    ULOffset = 0
   )
 )
 
-q=pickupList[[1]]
-print (q$getRawPlot())
+q <- pickupList[[1]]
+print(q$getRawPlot())
 
-for (p in pickupList) {
+for (p in pickupList){
   p$printData()
   #print (p$getRawPlot())
-  print (p$getIntPlot(min=-70, max=-50))
+  print(p$getIntPlot(min = -70, max = -50))
   #print (p$getRelPlot())
 }
 
 print(loadedAbsPlot(
   pickupList,
   chartHeading = chartHeading,
-  smoothing=0.05,
+  smoothing = 0.05,
   min = -70,
   max = -50
 ))
@@ -62,7 +63,6 @@ print(loadedRelPlot(
   pickupList,
   chartHeading = chartHeading,
   min = -4,
-  smoothing=0.08,
+  smoothing = 0.08,
   max = 4
 ))
-
